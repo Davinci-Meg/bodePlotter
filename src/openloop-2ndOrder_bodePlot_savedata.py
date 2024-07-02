ac60d97eaf07b4206318abd9ead95e42d5874fe1
Megumu Isshiki <r119110@g.kobe-kosen.ac.jp>
2024-07-02 15:34:02 +0900

    update

--- src/openloop-2ndOrder_bodePlot_savedata.py
+++ src/openloop-2ndOrder_bodePlot_savedata.py
@@ -16,6 +16,9 @@ ax2 = fig.add_subplot(2, 1, 2)
 P = pyctrl.tf([omega_n**2],[1, 2*zeta*omega_n, omega_n**2])
 gain, phase, w = pyctrl.bode(P, pyctrl.logspace(1,8), plot=False)
 
+# calculate frequency ratio
+frequency_ratio = w / omega_n
+
 # plot bode plot
 # set your own coordinates
 ax1.plot(w/(2*np.pi), 20*np.log10(gain), label='$\omega_n$=%.3f' % omega_n)
@@ -37,18 +40,23 @@ fig.tight_layout()
 
 # save as png
 fig.savefig('src/openloop-2ndOrder_bodePlot.png')
+print('openloop-2ndOrder_bodePlot.png is saved in src folder.')
 
 # Save data to CSV files
-# Data for ax1
+# Data for ax1 (Magnitude)
 with open('src/ax1_data.csv', 'w', newline='') as file:
     writer = csv.writer(file)
-    writer.writerow(['Frequency (Hz)', 'Magnitude (dB)'])
-    for freq, mag in zip(w/(2*np.pi), 20*np.log10(gain)):
-        writer.writerow([freq, mag])
+    writer.writerow(['Frequency (Hz)', 'Frequency Ratio', 'Magnitude (dB)'])
+    for freq, ratio, mag in zip(w/(2*np.pi), frequency_ratio, 20*np.log10(gain)):
+        writer.writerow([freq, ratio, mag])
 
-# Data for ax2
+    print('ax1_data.csv is saved in src folder.')
+
+# Data for ax2 (Phase)
 with open('src/ax2_data.csv', 'w', newline='') as file:
     writer = csv.writer(file)
-    writer.writerow(['Frequency (Hz)', 'Phase (degrees)'])
-    for freq, ph in zip(w/(2*np.pi), phase*180/np.pi):
-        writer.writerow([freq, ph])
+    writer.writerow(['Frequency (Hz)', 'Frequency Ratio', 'Phase (degrees)'])
+    for freq, ratio, ph in zip(w/(2*np.pi), frequency_ratio, phase*180/np.pi):
+        writer.writerow([freq, ratio, ph])
+
+    print('ax2_data.csv is saved in src folder.')
